cce773af7192241f34b4ae27b6893e7295b250e1
murilochag <murilochagas10@outlook.com>
2023-04-09 11:16:24 -0300

    conceitos iniciais

---
+++ kotlin/src/main/kotlin/Variaveis_exercicio.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    // declare uma variável mutável com capaz de armazenar seu nome comple
+    var nomeCompleto: String = "Murilo das Chagas de Carvalho"
+
+    // declare uma variavel de texto sem valor algum
+    var texto: String
+
+    // declare uma variável imutavel com o menor tipo de dado possivel para armazenar o numer de um calçado
+    var numeroCalcado: Byte = 40
+
+    // declare uma variável capaz de armazenar o pib do Brazil
+    val pibBrazil: Double = 1869000000000.0
+
+    // Declare uma variável capaz de armazenar a população do Brazil
+    val populacaoBrazil: Int = 211000000
+
+    // imprema o valo do PIB per capita
+    var pibPerCapita: Double = pibBrazil/populacaoBrazil
+    print(pibPerCapita)
+
+}
---
+++ kotlin/src/main/kotlin/conceitos_iniciais.kt
@@ -0,0 +1,9 @@
+fun main(){
+
+    val nome: String = "murilo"
+    val nome2 = "paula"
+    var nome3 = "outo nome"
+    var nome4: String = "outro nome"
+
+    print("nome: $nome")
+}
